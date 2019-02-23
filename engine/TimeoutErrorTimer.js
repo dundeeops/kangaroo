744da392477c44863f09f9165f76e9dce6bb0a6e
Максим Карпов <makame@mail.ru>
2019-02-23 21:58:16 +0100

    Clean TimeoutErrorTimer

--- engine/TimeoutErrorTimer.js
+++ engine/TimeoutErrorTimer.js
@@ -1,29 +1,63 @@
 const TIMEOUT = 10000;
 const TIMEOUT_MESSAGE = "TIMEOUT ERROR";
 
+const defaultOptions = {
+    onError: null,
+    message: TIMEOUT_MESSAGE,
+    timeout: TIMEOUT,
+    inject: {
+        _setTimeout: setTimeout,
+        _clearTimeout: clearTimeout,
+    },
+};
+
 module.exports = class TimeoutErrorTimer {
-    constructor(options) {
-        this._timeout = null;
-        this._timeoutInterval = (options && options.timeout) || TIMEOUT;
-        this._timeoutMessage = (options && options.message) || TIMEOUT_MESSAGE;
+    constructor(_options) {
+        const options = {
+            ...defaultOptions,
+            ..._options,
+        };
+
+        this._timeoutInterval = options.timeout;
+        this._timeoutMessage = options.message;
         this._onError = options && options.onError;
+
+        this.initInjections(options);
+
+        this.init(options);
     }
 
-    start(message, timeout) {
+    initInjections(options) {
+        this._setTimeout = options.inject._setTimeout;
+        this._clearTimeout = options.inject._clearTimeout;
+    }
+
+    init() {
+        this._timeout = null;
+    }
+
+    start(message, timeout = this._timeoutInterval) {
         if (!this._timeout) {
-            this._timeout = setTimeout(() => {
-                if (this._onError) {
-                    this._onError(Error(message || this._timeoutMessage));
-                } else {
-                    throw Error(message || this._timeoutMessage);
-                }
-            }, timeout || this._timeoutInterval);
+            this._timeout = this._setTimeout(() => {
+                this.onTimeout(message);
+            }, timeout);
+        }
+    }
+
+    onTimeout(_message) {
+        const message = _message || this._timeoutMessage;
+        const error = Error(message);
+
+        if (this._onError) {
+            this._onError(error);
+        } else {
+            throw error;
         }
     }
 
     stop() {
         if (this._timeout) {
-            clearTimeout(this._timeout);
+            this._clearTimeout(this._timeout);
         }
         this._timeout = null;
     }
