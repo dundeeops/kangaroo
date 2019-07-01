42b87de5eb8b6dd191103ab7f3243797dc8bf332
mkarpov <mkarpov@griddynamics.com>
2019-07-02 00:29:58 +0200

    test queue

--- examples/big/test.js
+++ examples/big/test.js
@@ -7,15 +7,15 @@ async function run() {
     let income = 0;
     let outcome = 0;
 
-    // for (let index = 0; index < 100000; index++) {
-    //     await q.push('test_' + index);
-    //     income++;
-    // }
+    for (let index = 0; index < 100000; index++) {
+        await q.push('test_' + index);
+        income++;
+    }
 
     for (let index = 0; index < 100005; index++) {
         const data = await q.pop();
         outcome++;
-        // console.log(data);
+        console.log(data);
     }
 
     console.log(income, outcome);
