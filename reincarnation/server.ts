14416275f7718dd7218c726a881b778e16054690
mkarpov <mkarpov@griddynamics.com>
2019-07-04 17:02:15 +0200

    Improves

--- reincarnation/server.ts
+++ reincarnation/server.ts
@@ -1,9 +1,10 @@
 import { of, from, timer, Observable, throwError, combineLatest } from "rxjs";
-import { map, tap, switchMap, retryWhen, delayWhen, mergeMap, share, concat, concatMap } from "rxjs/operators";
+import { map, tap, switchMap, retryWhen, delayWhen, mergeMap, share, concat, concatMap, find } from "rxjs/operators";
 import net from "net";
 import domain from "domain";
 import path from "path";
 import { SplitTransformStream } from "./splitTransformStream";
+import { Writable } from "stream";
 import { getHash } from "./serializationUtil";
 import { queueDuplexStream } from "./queueDuplexStream";
 
@@ -13,28 +14,51 @@ interface IDataBase {
 
 type IData = string | number | boolean | IDataBase | Array<string | number | boolean | IDataBase>;
 
-type IStage = (key: string, send: (stage: string, key: string, data: IData) => Promise<void>) => Promise<void>
+type IStageFunction = [
+  (data: IData) => Promise<void>,
+  () => void
+] | ((data: IData) => Promise<void>)
+
+type IStage = (key: string, send: (stage: string, key: string, data: IData) => Promise<void>) => Promise<IStageFunction>
 
 interface IStages {
   [key: string]: IStage;
 }
 
 function makeServer({
+  key,
   port,
   hostname,
-  onSocket,
+  queueDir,
+  queueLimit,
+  onData,
   onConnect,
   onError,
-  onClose
+  onClose,
 }: {
+  key: string;
   port: number;
   hostname: string;
-  onSocket: (socket: net.Socket) => void;
+  queueDir: string;
+  queueLimit?: number;
+  onData: (key: string, socket: net.Socket, data: IData) => Promise<void>;
   onConnect: Function;
   onError: Function;
   onClose: Function;
 }): net.Server {
-  const server = net.createServer(onSocket);
+  const server = net.createServer(socket => {
+    socket
+      .pipe(new SplitTransformStream())
+      .pipe(queueDuplexStream({
+        readableStream: socket,
+        concurrency: 10,
+        dir: queueDir,
+        fn: async (line) => {
+          await onData(key, socket, JSON.parse(line));
+        },
+        memoryLimit: queueLimit || 1000,
+      }))
+  });
   server.on("close", () => onClose());
   server.on("error", (error) => onError(error));
   server.on("listening", () => onConnect());
@@ -42,19 +66,67 @@ function makeServer({
   return server;
 }
 
-async function connectServer({
+function makeConnection({
+  key,
   port,
   hostname,
-  onSocket,
+  onData,
+  onConnect,
+  onError,
+  onClose,
 }: {
+  key: string;
   port: number;
   hostname: string;
-  onSocket: (socket: net.Socket) => void;
+  onData: (key: string, socket: net.Socket, data: IData) => Promise<void>;
+  onConnect: Function;
+  onError: Function;
+  onClose: Function;
+}) {
+  const socket = new net.Socket();
+  socket
+    .pipe(new SplitTransformStream())
+    .pipe(new Writable({
+      async write(data, encoding, cb) {
+        await onData(key, socket, JSON.parse(data.toString()));
+        cb();
+      },
+      final(cb) {
+        cb();
+      }
+    }));
+  socket.on("close", () => onClose());
+  socket.on("ready", () => onConnect());
+  socket.on("error", (error) => onError(error));
+  socket.connect({
+    host: hostname,
+    port: port,
+  });
+  return socket;
+}
+
+async function runServer({
+  key,
+  port,
+  hostname,
+  queueDir,
+  queueLimit,
+  onData,
+}: {
+  key: string;
+  port: number;
+  hostname: string;
+  queueDir: string;
+  queueLimit?: number;
+  onData: (key: string, socket: net.Socket, data: IData) => Promise<void>;
 }): Promise<net.Server> {
   return new Promise((r, e) => makeServer({
+    key,
     port,
     hostname,
-    onSocket,
+    queueDir,
+    queueLimit,
+    onData,
     onConnect() {
       r();
     },
@@ -67,7 +139,35 @@ async function connectServer({
   }));
 }
 
-export const retryStrategy = <T>({
+async function runConnection({
+  key,
+  port,
+  hostname, 
+  onData,
+}: {
+  key: string;
+  port: number;
+  hostname: string;
+  onData: (key: string, socket: net.Socket, data: IData) => Promise<void>;
+}): Promise<net.Socket> {
+  return new Promise((r, e) => makeConnection({
+    key,
+    port,
+    hostname,
+    onData,
+    onConnect() {
+      r();
+    },
+    onError(error) {
+      e(error);
+    },
+    onClose() {
+      e();
+    },
+  }));
+}
+
+export const retryServerStrategy = <T>({
   maxRetryAttempts = 3,
   scalingDuration = 1000,
 }: {
@@ -81,8 +181,7 @@ export const retryStrategy = <T>({
       return throwError(error);
     }
     console.error(
-      `Attempt ${retryAttempt}: retrying in ${retryAttempt *
-        scalingDuration}ms`
+      `Attempt ${retryAttempt}: retrying in ${retryAttempt * scalingDuration}ms`
     );
     return timer(retryAttempt * scalingDuration);
   }),
@@ -100,7 +199,7 @@ function runDomain$<T>(run: () => Promise<T>) {
       });
     })).pipe(
       retryWhen(
-        retryStrategy<T>({
+        retryServerStrategy<T>({
           maxRetryAttempts: 3,
           scalingDuration: 1000,
         }),
@@ -110,50 +209,196 @@ function runDomain$<T>(run: () => Promise<T>) {
 }
 
 function runDomainServer$({
-  port,
-  hostname,
-  onSocket,
-}: {
-  port: number;
-  hostname: string;
-  onSocket: (socket: net.Socket) => void;
-}) {
-  return runDomain$(async () => await connectServer({
-    hostname,
-    port,
-    onSocket,
-  }));
-}
-
-function runServer$({
+  key,
   port,
   hostname,
   queueDir,
+  queueLimit,
   onData,
 }: {
+  key: string;
   port: number;
   hostname: string;
   queueDir: string;
-  onData: (socket: net.Socket, data: IData) => Promise<void>;
+  queueLimit?: number;
+  onData: (key: string, socket: net.Socket, data: IData) => Promise<void>;
+}) {
+  return runDomain$(
+    async () => await runServer({
+      key,
+      hostname,
+      port,
+      queueDir,
+      queueLimit,
+      onData,
+    }),
+  );
+}
+
+function runServer$({
+  key,
+  port,
+  hostname,
+  queueDir,
+  queueLimit,
+  onData,
+}: {
+  key: string;
+  port: number;
+  hostname: string;
+  queueDir: string;
+  queueLimit?: number;
+  onData: (key: string, socket: net.Socket, data: IData) => Promise<void>;
 }) {
   return runDomainServer$({
+    key,
     hostname,
     port,
-    onSocket: (socket) => {
-      socket
-        .pipe(new SplitTransformStream())
-        .pipe(queueDuplexStream({
-          readableStream: socket,
-          concurrency: 10,
-          dir: queueDir,
-          fn: async (line) => {
-            await onData(socket, JSON.parse(line));
-          },
-          memoryLimit: 1000,
-        }))
-    },
+    queueDir,
+    queueLimit,
+    onData,
   });
 }
+
+export const retryConnectionStrategy = <T>({
+  maxRetryAttempts = 3,
+  scalingDuration = 1000,
+  timeout = 30000,
+}: {
+  maxRetryAttempts?: number,
+  scalingDuration?: number,
+  timeout?: number,
+} = {}) => (attempts: Observable<T>) => attempts.pipe(
+  mergeMap((error, i) => {
+    const retryAttempt = i + 1;
+    console.error(error);
+    if (retryAttempt % maxRetryAttempts === 0) {
+      return timer(timeout);
+    }
+    console.error(
+      `Attempt ${retryAttempt}: retrying to reconnect in ${retryAttempt * scalingDuration}ms`
+    );
+    return timer(retryAttempt * scalingDuration);
+  }),
+);
+
+function runConnection$({
+  key,
+  port,
+  hostname,
+  onData,
+}: {
+  key: string;
+  port: number;
+  hostname: string;
+  onData: (key: string, socket: net.Socket, data: IData) => Promise<void>;
+}) {
+  return from(
+    runConnection({
+      key,
+      port,
+      hostname,
+      onData,
+    }),
+  ).pipe(
+    retryWhen(
+      retryConnectionStrategy({
+        maxRetryAttempts: 3,
+        scalingDuration: 1000,
+        timeout: 30000,
+      }),
+    ),
+  );
+}
+
+function runPairConnection$({
+  key,
+  worker,
+  manager,
+}: {
+  key: string;
+  worker: {
+    port: number;
+    hostname: string;
+    onData: (key: string, socket: net.Socket, data: IData) => Promise<void>;
+  };
+  manager: {
+    port: number;
+    hostname: string;
+    onData: (key: string, socket: net.Socket, data: IData) => Promise<void>;
+  }
+}) {
+  return combineLatest([
+    of(key),
+    runConnection$({ key, ...manager }),
+    runConnection$({ key, ...worker }),
+  ]);
+}
+
+function runPairServer$({
+  key,
+  worker,
+  manager,
+  onDataManager,
+  onDataWorker,
+}: {
+  key: string;
+  onDataManager: (key: string, socket: net.Socket, data: IData) => Promise<void>;
+  onDataWorker: (key: string, socket: net.Socket, data: IData) => Promise<void>;
+  worker: {
+    port: number;
+    hostname: string;
+    queueDir: string;
+    queueLimit?: number;
+  };
+  manager: {
+    port: number;
+    hostname: string;
+    queueDir: string;
+    queueLimit?: number;
+  }
+}) {
+  return combineLatest([
+    runServer$({ key, onData: onDataManager, ...manager }),
+    runServer$({ key, onData: onDataWorker, ...worker }),
+  ]);
+}
+
+function runConnections$({
+  onDataConnectionManager,
+  onDataConnectionWorker,
+  connections,
+}: {
+  onDataConnectionManager: (key: string, socket: net.Socket, data: IData) => Promise<void>;
+  onDataConnectionWorker: (key: string, socket: net.Socket, data: IData) => Promise<void>;
+  connections: {
+    key: string;
+    manager: {
+      port: number;
+      hostname: string;
+    };
+    worker: {
+      port: number;
+      hostname: string;
+    };
+  }[];
+}) {
+  return combineLatest(
+    connections.map(config => runPairConnection$({
+      key: config.key,
+      manager: {
+        ...config.manager,
+        onData: onDataConnectionManager,
+      },
+      worker: {
+        ...config.worker,
+        onData: onDataConnectionWorker,
+      },
+    }))
+  );
+}
+
+
 
 // function runServerData$({
 //   port,
@@ -189,25 +434,25 @@ function runServer$({
 //   });
 // }
 
-function convertCallbackData$<T>(
-  observable: (callback: (onCallbackData: () => Promise<T>) => Promise<void>) => Observable<any>
-) {
-  return new Observable<T>(o => {
-    const observable$ = observable(
-      async (onCallbackData) => {
-        o.next(await onCallbackData());
-      }
-    ).subscribe({
-      error(error) {
-        o.error(error);
-      },
-      complete() {
-        o.complete();
-      },
-    });
-    o.add(() => observable$.unsubscribe());
-  });
-}
+// function convertCallbackData$<T>(
+//   observable: (callback: (onCallbackData: () => Promise<T>) => Promise<void>) => Observable<any>
+// ) {
+//   return new Observable<T>(o => {
+//     const observable$ = observable(
+//       async (onCallbackData) => {
+//         o.next(await onCallbackData());
+//       }
+//     ).subscribe({
+//       error(error) {
+//         o.error(error);
+//       },
+//       complete() {
+//         o.complete();
+//       },
+//     });
+//     o.add(() => observable$.unsubscribe());
+//   });
+// }
 
 // function runWorkerData$({
 //   port,
@@ -265,6 +510,8 @@ interface IProcessing {
   usedGroupsTotals: {},
 }
 
+type IProcessingState = Map<string, IProcessing>;
+
 function getDefaultProcessingMap(): IProcessing {
   return {
     totalSum: null,
@@ -276,90 +523,177 @@ function getDefaultProcessingMap(): IProcessing {
   }
 }
 
-async function send(session, group, stage, key, data) {
-  const connectionKey = await getSessionStageKeyConnectionScript(
-      session,
-      stage,
-      key,
-  );
-  if (!connectionKey || _key === connectionKey + "DISABLED") {
-      await onData(null, {
-          session,
-          group,
-          stage,
-          key,
-          data,
-      });
-  } else {
-      await sendToServer(
-          connectionKey,
-          session,
-          group,
-          stage,
-          key,
-          data,
-      );
-  }
-  return connectionKey;
+// async function send(session, group, stage, key, data) {
+//   const connectionKey = await getSessionStageKeyConnectionScript(
+//       session,
+//       stage,
+//       key,
+//   );
+//   if (!connectionKey || _key === connectionKey + "DISABLED") {
+//       await onData(null, {
+//           session,
+//           group,
+//           stage,
+//           key,
+//           data,
+//       });
+//   } else {
+//       await sendToServer(
+//           connectionKey,
+//           session,
+//           group,
+//           stage,
+//           key,
+//           data,
+//       );
+//   }
+//   return connectionKey;
+// }
+
+// function getSendCatchUsedGroupWrap(group, session) {
+//   return async (stage, key, data) => {
+//     const nextGroup = getHash(group, stage);
+//     setUsedGroup(group, nextGroup);
+//     increaseUsedGroupSend(group, nextGroup);
+//     await send(session, nextGroup, stage, key, data);
+//   };
+// }
+
+// async function getProcessingStorageMap(group, session, key, mapper): Promise<IProcessingStorageMap> {
+//   const sendWrap = getSendCatchUsedGroupWrap(group, session);
+//   const mapResult = await mapper(key, sendWrap);
+//   const mapCouple = parseMapperResult(mapResult);
+//   return {
+//     onData: mapCouple[0],
+//     onFinish: mapCouple[1],
+//   };
+// }
+
+enum QuestionTypeEnum {
+  GET_SESSION_STAGE_KEY_SERVER = "getSessionStageKeyServer",
+  COUNT_PROCESSED = "countProcessed",
+  CAN_GET_STAGE = "canGetStage",
 }
 
-function getSendCatchUsedGroupWrap(group, session) {
-  return async (stage, key, data) => {
-    const nextGroup = getHash(group, stage);
-    setUsedGroup(group, nextGroup);
-    increaseUsedGroupSend(group, nextGroup);
-    await send(session, nextGroup, stage, key, data);
-  };
+enum NotificationTypeEnum {
+  END_PROCESSING = "endProcessing",
+  NULL_ACHIEVED = "nullAchieved",
 }
 
-async function getProcessingStorageMap(group, session, key, mapper): Promise<IProcessingStorageMap> {
-  const sendWrap = getSendCatchUsedGroupWrap(group, session);
-  const mapResult = await mapper(key, sendWrap);
-  const mapCouple = parseMapperResult(mapResult);
-  return {
-    onData: mapCouple[0],
-    onFinish: mapCouple[1],
-  };
+interface IMachineState {
+  ask?: (question: QuestionTypeEnum, data: IData) => Promise<IData>;
+  notify?: (type: NotificationTypeEnum, data: IData) => Promise<void>;
+  send?: (workerKey: string, data: {
+    session: string;
+    group: string;
+    stage: string;
+    key?: string;
+    data: IData;
+  }) => Promise<void>;
 }
 
-function runWorker$({
-  port,
-  hostname,
-  stages,
-}: {
-  port: number;
-  hostname: string;
+interface IConfiguration {
+  key: string;
+  manager: {
+    hostname: string;
+    port: number;
+    queueDir: string;
+  },
+  worker: {
+    hostname: string;
+    port: number;
+    queueDir: string;
+  },
+  connections: {
+    key: string;
+    manager: {
+      hostname: string;
+      port: number;
+    };
+    worker: {
+      hostname: string;
+      port: number;
+    };
+  }[],
   stages: IStages;
-}) {
-  return of<Map<string, IProcessing>>(new Map()).pipe(
-    concatMap(maps => combineLatest([
-      of(maps),
-      of(async (socket: net.Socket, data: IData) => {
+}
 
+interface IServerState {
+  processingState: IProcessingState;
+  machineState: IMachineState;
+}
+
+function runMachine$(configuration: IConfiguration) {
+  return of<IServerState>({
+    processingState: new Map(),
+    machineState: {},
+  }).pipe(
+    mergeMap(state => combineLatest([
+      of(state),
+      runPairServer$({
+        key: configuration.key,
+        manager: configuration.manager,
+        worker: configuration.worker,
+        onDataManager: async (key, socket, data) => {
+
+        },
+        onDataWorker: async (key, socket, data) => {
+
+        },
+      }),
+      runConnections$({
+        connections: configuration.connections,
+        onDataConnectionManager: async (key, socket, data) => {
+
+        },
+        onDataConnectionWorker: async (key, socket, data) => {
+
+        },
       }),
     ])),
-    concatMap(([maps, onData]) => combineLatest([
-      of(maps),
-      runServer$({
-        hostname,
-        port,
-        queueDir: path.resolve("./data_queue"),
-        onData,
-      })
-    ])),
-    tap(a => console.log(a))
+    map(([state, server, connections]) => {
+      state.machineState.ask = async (question, data) => {
+        switch(question) {
+          case QuestionTypeEnum.CAN_GET_STAGE:
+            return true;
+          case QuestionTypeEnum.COUNT_PROCESSED:
+            return 0;
+          case QuestionTypeEnum.GET_SESSION_STAGE_KEY_SERVER:
+            return true;
+        }
+        return null;
+      };
+
+      state.machineState.notify = async (type, data) => {
+        switch(type) {
+          case NotificationTypeEnum.NULL_ACHIEVED: 
+            return;
+          case NotificationTypeEnum.END_PROCESSING: 
+            return;
+        }
+      };
+
+      state.machineState.send = async (serverKey, data) => {
+        const [,,worker] = connections.find(([key]) => key === serverKey);
+        worker.push(JSON.stringify(data));
+      };
+
+      return true;
+    }),
   );
 }
 
-of({
+runMachine$({
   key: 'local',
   worker: {
     hostname: '0.0.0.0',
     port: 3000,
+    queueDir: path.resolve("./data_queue"),
   },
   manager: {
     hostname: '0.0.0.0',
     port: 3001,
+    queueDir: path.resolve("./manager_queue"),
   },
   connections: [
     {
@@ -374,11 +708,11 @@ of({
       },
     }
   ],
-  mapping: {
-    init: (key, send) => {
+  stages: {
+    init: async (key, send) => {
       let state = false;
       return [
-        async ({ data }) => {
+        async (data) => {
           state = !state;
           await send("reduce_2_flows", state ? "final" : "final_alt", data);
         },
@@ -387,9 +721,9 @@ of({
         },
       ];
     },
-    reduce_2_flows: (key, send) => {
+    reduce_2_flows: async (key, send) => {
       return [
-        async ({ data }) => {
+        async (data) => {
           await send("map", null, data);
         },
         () => {
@@ -397,9 +731,9 @@ of({
         },
       ];
     },
-    map: (key, send) => {
+    map: async (key, send) => {
       return [
-        async ({ data }) => {
+        async (data) => {
           await send("final_reduce", "final", data);
           // console.log("map", kkk++);
         },
@@ -408,11 +742,11 @@ of({
         },
       ];
     },
-    final_reduce: (key) => {
+    final_reduce: async (key) => {
       let sum = 0;
       const timeStarted = +new Date();
       return [
-        async ({ data }) => {
+        async (data) => {
           sum++;
           console.log("final_reduce", sum);
         },
@@ -425,6 +759,6 @@ of({
       ];
     }
   }
-}).pipe(
-  map(conf => runWorker$(conf))
-).toPromise();
+}).subscribe(state => {
+  console.log(state);
+});
